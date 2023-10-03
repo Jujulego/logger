6f07362b7812217d16ad966994b5409d0fa3dcb4
Julien Capellari <julien.capellari@gmail.com>
2023-10-03 19:21:24 +0200

    test: console transport

--- tests/transports/to-console.test.ts
+++ tests/transports/to-console.test.ts
@@ -1,12 +1,39 @@
 import chalk from 'chalk-template';
-import { vi } from 'vitest';
+import { beforeEach, vi } from 'vitest';
 
 import { logger$ } from '@/src/logger.js';
 import { toConsole } from '@/src/transports/to-console.js';
 
+// Setup
+beforeEach(() => {
+  vi.restoreAllMocks();
+});
+
 // Tests
 describe('toConsole', () => {
-  it('should print log in console', () => {
+  it('should print error log using console.error', () => {
+    vi.spyOn(console, 'error').mockReturnValue();
+
+    const logger = logger$();
+    logger.subscribe(toConsole().next);
+    logger.error('life');
+
+    // eslint-disable-next-line no-console
+    expect(console.error).toHaveBeenCalledWith('life');
+  });
+
+  it('should print info log using console.warn', () => {
+    vi.spyOn(console, 'warn').mockReturnValue();
+
+    const logger = logger$();
+    logger.subscribe(toConsole().next);
+    logger.warning('life');
+
+    // eslint-disable-next-line no-console
+    expect(console.warn).toHaveBeenCalledWith('life');
+  });
+
+  it('should print info log using console.log', () => {
     vi.spyOn(console, 'log').mockReturnValue();
 
     const logger = logger$();
@@ -15,6 +42,28 @@ describe('toConsole', () => {
 
     // eslint-disable-next-line no-console
     expect(console.log).toHaveBeenCalledWith('life');
+  });
+
+  it('should print verbose log using console.log', () => {
+    vi.spyOn(console, 'log').mockReturnValue();
+
+    const logger = logger$();
+    logger.subscribe(toConsole().next);
+    logger.verbose('life');
+
+    // eslint-disable-next-line no-console
+    expect(console.log).toHaveBeenCalledWith('life');
+  });
+
+  it('should print debug log using console.debug', () => {
+    vi.spyOn(console, 'debug').mockReturnValue();
+
+    const logger = logger$();
+    logger.subscribe(toConsole().next);
+    logger.debug('life');
+
+    // eslint-disable-next-line no-console
+    expect(console.debug).toHaveBeenCalledWith('life');
   });
 
   it('should print log in console including label and timestamp', () => {
