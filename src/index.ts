971248dc32a82a051e48c74990bb6c5d399cc304
Julien Capellari <julien.capellari@gmail.com>
2023-12-28 19:37:31 +0100

    feat: add debug filter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './attributes/index.js';
 export * from './defs/index.js';
+export * from './filters/index.js';
 export * from './formats/index.js';
 export * from './logger.js';
 export * from './quick.js';
---
+++ tests/filters/debug.test.ts
@@ -0,0 +1,3 @@
+import { debugFilter } from '@/src/filters/debug.js';
+
+// Method
